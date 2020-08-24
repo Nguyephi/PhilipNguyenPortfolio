5cc4f343637297bfdec3bbb8e37e4a766dd732e4
Phil <pronewgen92@gmail.com>
2020-08-23 22:06:06 -0400

    7th commit. Add header with auto typing feature.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Typist from 'react-typist';
+
+import '../css/header.css'
+import headerFullBackground from '../img/Philip_Nguyen_Portfolio_Header.jpg';
 
 export default function Header() {
   return (
@@ -7,11 +11,14 @@ export default function Header() {
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#1B263B',
-      // backgroundImage: `url(${headerFullBackground})`,
+      backgroundImage: `url(${headerFullBackground})`,
       backgroundSize: 'cover',
       width: '100%',
       height: '40rem'
     }}>
+      <Typist avgTypingSpeed={60} startDelay={1000} cursor={{ hideWhenDone: true }}>
+        <h1 style={{ display: 'inline-block' }}>Software Engineer</h1>
+      </Typist>
     </div>
   )
 }
