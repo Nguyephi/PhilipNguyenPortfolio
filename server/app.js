710e672221cfc116c1daf255a29c4a64ec79e55f
Philip Nguyen <pronewgen92@gmail.com>
2020-09-06 12:32:43 -0400

    Comment out mailjet

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ import App from '../client/src/components/App.jsx';
 import { ServerStyleSheets, ThemeProvider } from '@material-ui/core/styles';
 import theme from '../client/src/utils/theme.js'
 import Models from './models/';
-import mailjet from 'node-mailjet';
+// import mailjet from 'node-mailjet';
 
-mailjet.connect('437b0575f7a8c2c5357aba7deb08e22b', 'dad3a8d10343f38fb9cf558de8f82665');
+// mailjet.connect(process.env MJ_API, process.env.MJ_SECRET_KEY);
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -60,35 +60,35 @@ app.post('/api/sendemail', (req, res) => {
         if (err) {
             res.status(400).send(err)
         }
-        const request = mailjet
-            .post("send", { 'version': 'v3.1' })
-            .request({
-                "Messages": [
-                    {
-                        "From": {
-                            "Email": result[0].email,
-                            "Name": result[0].name
-                        },
-                        "To": [
-                            {
-                                "Email": "pronewgen92@gmail.com",
-                                "Name": "Philip N."
-                            }
-                        ],
-                        "Subject": result[0].title,
-                        "TextPart": result[0].message,
-                    }
-                ]
-            })
-        request
-            .then((result) => {
-                res.status(200).json({
-                    status: result.body.Messages[0].Status
-                })
-            })
-            .catch((err) => {
-                console.log(err.statusCode)
-            })
+//         const request = mailjet
+//             .post("send", { 'version': 'v3.1' })
+//             .request({
+//                 "Messages": [
+//                     {
+//                         "From": {
+//                             "Email": result[0].email,
+//                             "Name": result[0].name
+//                         },
+//                         "To": [
+//                             {
+//                                 "Email": "pronewgen92@gmail.com",
+//                                 "Name": "Philip N."
+//                             }
+//                         ],
+//                         "Subject": result[0].title,
+//                         "TextPart": result[0].message,
+//                     }
+//                 ]
+//             })
+//         request
+//             .then((result) => {
+//                 res.status(200).json({
+//                     status: result.body.Messages[0].Status
+//                 })
+//             })
+//             .catch((err) => {
+//                 console.log(err.statusCode)
+//             })
     })
 })
 
